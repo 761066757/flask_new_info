0dbd5af9766f6d1285ac1b6b0add3f91a4a057f5
761066757 <761066757@qq.com>
2020-06-22 18:48:05 +0800

    点击排行

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -10,6 +10,8 @@ from flask import render_template
 from . import index_blu
 from flask import render_template, current_app, session
 from info.models import User
+from info import constants
+from info.models import User, News
 
 @index_blu.route('/')
 def index():
@@ -18,11 +20,26 @@ def index():
     user_id = session.get("user_id")
     # 通过id查询用户信息
     user = None
+    news_list = None
     if user_id:
         try:
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
+
+        try:
+            news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+        except Exception as e:
+            current_app.logger.error(e)
+        click_news_list = []
+        for news in news_list if news_list else []:
+            click_news_list.append(news.to_basic_dict())
+        data = {
+            "user_info": user.to_dict() if user else None,
+            "click_news_list": click_news_list,
+        }
+
+        return render_template('news/index.html', data=data)
 
     return render_template('news/index.html', data={"user_info": user.to_dict() if user else None})
 
